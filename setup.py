59586ffecb17fe8d54236b1cc15e6f669f74a42a
Christopher Lockwood <christopher.lockwood13@imperial.ac.uk>
2017-01-10 03:48:43 +0000

    Searches for Vundle install, elsewise installs vundle and initiates vim plugin install

--- setup.py
+++ setup.py
@@ -3,6 +3,24 @@ import os
 def copy(src, dest):
     os.system('cp ' + src + ' ' + dest)
 
+print('Copying vimrc')
 copy('./vimrc', '~/.vimrc')
+print('Copying bashrc')
 copy('./bashrc', '~/.bashrc')
+print('Copying tmux')
 copy('./tmux', '~/.tmux.conf')
+
+# Check if there is a vim config
+def syscall(string):
+    return os.system(string)
+
+if syscall('test -d ~/.vim') != 0:
+    if syscall('test -d ~/.vim/bundle') != 0:
+        if syscall('test -f ~/.vim/bundle/Vundle.vim') != 0:
+            print('Installing Vundle')
+            print(syscall('git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim'))
+            
+print('Installing vim Plugins')
+syscall('vim +PluginInstall +qall')
+
+print('Done')
