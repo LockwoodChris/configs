1c6d280a792864f074b92e7dcba097131ee99193
Christopher Lockwood <chris-lockwood@hotmail.co.uk>
2017-05-19 12:24:51 +0100

    TPM auto installed, auto updates and installs vim and tmux plugins, removed teamocil

--- bash_profile
+++ bash_profile
@@ -1,4 +1,3 @@
 alias wow="git status"
 alias such="git"
-export PATH=$PATH:~/Android/Sdk/tools/:~/Android/Sdk:~/Android/Sdk/platform-tools:$RUBY_HOME
-export ANDROID_HOME=~/Android/Sdk
+~/.bashrc
--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ def copy(src, dest):
 def exists(path):
     return os.path.exists(os.path.expanduser(path))
 
+# Check if there is a vim config
+def syscall(string):
+    return os.system(string)
+
 print('Copying vimrc')
 copy('./vimrc', '~/.vimrc')
 print('Copying bashrc')
@@ -15,31 +19,35 @@ print('Copying tmux')
 copy('./tmux', '~/.tmux.conf')
 print('Copying bash_profile')
 copy('./bash_profile', '~/.bash_profile')
+print('Creating bash_personal if not exists')
+syscall('touch ~/.bash_personal')
 
-# Check if there is a vim config
-def syscall(string):
-    return os.system(string)
-
-if exists('~/.vim'):
+if not exists('~/.vim'):
     syscall('mkdir ~/.vim')
 
-if exists('~/.vim/bundle'):
+if not exists('~/.vim/bundle'):
     syscall('mkdir ~/.vim/bundle')
 
-if exists('~/.vim/bundle/Vundle.vim'):
+if not exists('~/.vim/bundle/Vundle.vim'):
     print('Installing Vundle')
     print(syscall('git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim'))
+
+if not exists('~/.tmux/plugins/tpm'):
+    print(syscall('git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm'))
     
 print('Installing vim Plugins')
 print('----------------------')
 print('+ starting install   +')
 syscall('vim +PluginInstall +qall')
-
-print('Done')
+print('+ updating plugins   +')
+print('----------------------')
+syscall('vim +PluginUpdate +qall')
 
 print('Installing tmux Plugins')
-print('-----------------------')
-print('+ Installing teamocil +')
-syscall('gem install --user teamocil')
-if not exists('~/.teamocil'):
-    syscall('mkdir ~/.teamocil')
+print('----------------------')
+print(syscall('~/.tmux/plugins/tpm/bin/install_plugins'))
+print('----------------------')
+print('Updating tmux Plugins')
+print(syscall('~/.tmux/plugins/tpm/bin/update_plugins all'))
+
+print('Done')
